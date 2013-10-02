049c6c1299e7180221b14cc6bb8fd0876a5403e7
Kay Robbins <kay.robbins@utsa.edu>
2013-10-02 15:43:31 -0500

    Committed index.php

---
+++ namespaceTest/WebContent/index.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<h1>Several tests of namespaces</h1>
+
+<h3>Test 1:</h3>
+Calling an unqualified function in the same namespace:<br>
+<a href="./Library/myapp1.php">Click here</a>
+
+<h3>Test 2:</h3>
+Calling a qualified function from a different namespace:<br>
+<a href="./Library/myapp2.php">Click here</a><br>
+
+<h3>Test 3:</h3>
+Use autoloader rather than require:<br>
+<a href="./Library/myapp3.php">Click here</a><br>
+</body></html>
