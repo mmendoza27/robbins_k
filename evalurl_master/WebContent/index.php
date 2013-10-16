eecbb1b98449cbe457abe9d926744704220f79bc
Kay Robbins <kay.robbins@utsa.edu>
2013-10-16 11:02:27 -0500

    Put a few more error messages in index page in preparation for refactoring Front controller.

--- evalurl_master/WebContent/index.php
+++ evalurl_master/WebContent/index.php
@@ -1,9 +1,11 @@
 <?php  
 require_once (dirname(__FILE__). DIRECTORY_SEPARATOR . "autoload.php");
-echo "On index page";
-
+echo "On index page<br>";
+$v = $_SERVER['REQUEST_URI'];
+echo  "Request URI: $v <br>";
 $frontController = new FrontController();
 $frontController->run();
-echo "Front controller has run: request path is:";
-echo  $_SERVER['REQUEST_URI'] + "<br>";
+echo "Front controller has run: request path is:<br>";
+$v = $_SERVER['REQUEST_URI'];
+echo  "Request URI after: $v <br>";
 ?>
