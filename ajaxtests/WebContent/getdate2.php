763dc1d8aadb03df6053ccf69ec6b84da6168b4c
Kay Robbins <kay.robbins@utsa.edu>
2013-10-17 15:16:27 -0500

    Here are some basic examples using Ajax.

---
+++ ajaxtests/WebContent/getdate2.php
@@ -0,0 +1,4 @@
+<?php 
+    echo "Hello ". $_GET["fname"] ." " . $_GET["lname"] . "<br>";
+    echo "From the server the date is: " . date('F j\, Y ');
+?>
---
+++ ajaxtests/WebContent/getdate3.php
@@ -0,0 +1,4 @@
+<?php 
+    echo "Hello ". $_POST["fname"] ." " . $_POST["lname"] . "<br>";
+    echo "From the server the date is: " . date('F j\, Y ');
+?>
